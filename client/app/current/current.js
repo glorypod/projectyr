f32913c66c5d6a4d9a5a41a5ba5a34cc3c3f9f16
Ignacio Prado <ignacio@gatogordoweb.com>
2015-09-17 15:49:47 -0500

    Make clock assign time

--- client/app/current/current.js
+++ client/app/current/current.js
@@ -3,30 +3,53 @@
   .controller('CurrentController', CurrentController);
 
   function CurrentController ($scope, $interval, Project, Auth, $location) {
-    /* set up page */
+    $scope.setDefaults = function(){
+      $scope.running = false;
+      $scope.timeToAdd = 0;
+      $scope.started = false;
+      $scope.currentProject = $scope.projects[0];
+      $scope.totalElapsed = $scope.currentProject.act_time * 60 * 60 * 1000;
+    }
     $scope.init = function ()  {
       Project.getOpen()
         .then(function(all) {
           $scope.projects = all.projects;
-          $scope.running = false;
-          $scope.timeToAdd = 0;
-          $scope.currentProject = $scope.projects[0];
+          $scope.setDefaults();
         });
     }
     
     $scope.projects = {};
     $scope.currentProject = null;
-    $scope.running = false;
+    $scope.totalElapsed;
+    $scope.timeToAdd;
     $scope.init();
+    
+
 
     $scope.start = function() {
-      $scope.$broadcast('timer-start');
+      if(!$scope.started) {
+        $scope.$broadcast('timer-start');
+      } else {
+        $scope.$broadcast('timer-resume');
+      }
+      $scope.started = $scope.started || true;
       $scope.running = true;
     }
 
     $scope.stop = function() {
       $scope.$broadcast('timer-stop');
       $scope.running = false;
+    }
+
+    $scope.$on('timer-stopped', function(event, data){
+      $scope.timeToAdd += data.millis;
+    });
+
+    $scope.end = function(){
+      $scope.timeToAdd = Number(($scope.timeToAdd / (60 * 60 * 1000)).toFixed(2));
+      $scope.currentProject.act_time += $scope.timeToAdd;
+      $scope.$broadcast('timer-reset');
+      $scope.setDefaults();
     }
 
     $scope.timeAssign = function () {
