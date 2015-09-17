03af2238907d4220877f4f2bc08ceba6cef5bfb2
Ignacio Prado <ignacio@gatogordoweb.com>
2015-09-17 13:47:41 -0500

    Add digital clock font

--- client/app/current/current.js
+++ client/app/current/current.js
@@ -3,27 +3,51 @@
   .controller('CurrentController', CurrentController);
 
   function CurrentController ($scope, $rootScope, $interval, Project, Auth, $location) {
-    /* set up page */
-    $rootScope.init = function (project)  {
+    $scope.setDefaults = function(){
+      $scope.running = false;
+      $scope.timeToAdd = 0;
+      $scope.started = false;
+      $scope.currentProject = $rootScope.project || $scope.projects[0];
+      $scope.totalElapsed = $scope.currentProject.act_time * 60 * 60 * 1000;
+    }
+    $rootScope.init = function ()  {
       Project.getOpen()
         .then(function(all) {
           $scope.projects = all.projects;
-          $scope.running = false;
-          $scope.timeToAdd = 0;
-          $scope.currentProject = project || $scope.projects[0];
+          $scope.setDefaults();
         });
     }
 
+    $scope.projects = {};
+    $scope.currentProject = null;
+    $scope.totalElapsed;
+    $scope.timeToAdd;
     $rootScope.init();
-
+    
     $scope.start = function() {
-      $scope.$broadcast('timer-start');
+      if(!$scope.started) {
+        $scope.$broadcast('timer-start');
+      } else {
+        $scope.$broadcast('timer-resume');
+      }
+      $scope.started = $scope.started || true;
       $scope.running = true;
     }
 
     $scope.stop = function() {
       $scope.$broadcast('timer-stop');
       $scope.running = false;
+    }
+
+    $scope.$on('timer-stopped', function(event, data){
+      $scope.timeToAdd += data.millis;
+    });
+
+    $scope.end = function(){
+      $scope.timeToAdd = Number(($scope.timeToAdd / (60 * 60 * 1000)).toFixed(2));
+      $scope.currentProject.act_time += $scope.timeToAdd;
+      $scope.$broadcast('timer-reset');
+      $scope.setDefaults();
     }
 
     $scope.timeAssign = function () {
