01b37e8b8628c5ee56b2cc34aad14825af38d599
Vahagn Stepanyan <vahagnst@gmail.com>
2015-09-17 19:49:34 -0500

    dropdown refreshes automatically when new project is created

--- client/index.js
+++ client/index.js
@@ -3,12 +3,16 @@
   .controller('IndexController', IndexController);
 
   function IndexController ($scope, $rootScope, $interval, Project, Auth, $location) {
-
-    Project.getOpen()
+    
+    $rootScope.initIndex = function() {
+      Project.getOpen()
       .then(function(all) {
         $scope.projects = all.projects;
       });
+    }
 
+    $rootScope.initIndex();
+    
     $scope.setProject = function(project){
       $rootScope.project = project;
       $rootScope.init();
