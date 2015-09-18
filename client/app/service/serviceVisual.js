b2bb32f2fbe34b16570f7f3bb3c81ec67d829533
Elizabeth Matsusaka <ellie.matsusaka@gmail.com>
2015-09-17 19:12:08 -0500

    move graph options to factory

--- client/app/service/serviceVisual.js
+++ client/app/service/serviceVisual.js
@@ -1,30 +1,40 @@
-// currently used in open and closed. 
-// will be used in current to render data 
-
 (function(){
   
-  angular.module('projectyr.service', [])
+  angular.module('projectyr.service')
   .factory('Visual', Visual);
 
-  function Visual ($scope, $location, $window) {
+  function Visual ($location, $window) {
+    var options = {
+      responsive: true,
+      segmentShowStroke: true,
+      segmentStrokeColor: '#fff',
+      segmentStrokeWidth: 2,
+      percentageInnerCutout: 70, 
+      animationSteps: 100,
+      animationEasing: 'easeOutBounce',
+      animateRotate: true,
+      animateScale: false,
+      legendTemplate: '<ul class="tc-chart-js-legend"><% for (var i=0; i<segments.length; i++){%><li><span style="background-color:<%=segments[i].fillColor%>"></span><%if(segments[i].label){%><%=segments[i].label%><%}%></li><%}%></ul>'
+    };
 
-    function setSelectedProject = function(project) {
-
-    }
-
-    function createDonutData = function () {
-
-    }
-
-    function createBarData = function () {
-
-    }
-
-    var barOptions = {};
-
-    var options = {};
-
+    var barOptions = {
+      responsive: true,
+      scaleBeginAtZero : true,
+      scaleShowGridLines: false,
+      scaleGridLineColor: "rgba(0,0,0,.05)",
+      scaleGridLineWidth: 1,
+      barShowStroke: true,
+      barStrokeWidth: 2,
+      barValueSpacing: 5,
+      barDatasetSpacing: 10,
+      legendTemplate: '<ul class="tc-chart-js-legend"><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].fillColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>'
+    };
+    
+    return {
+      options: options,
+      barOptions: barOptions
+    };
   }
+})();
 
-// return obj of all functions in Visual
-});
+
